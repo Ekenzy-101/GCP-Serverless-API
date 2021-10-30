90b92812166051bc63e8d72d1137cd725a1da69c
Emmanuel Onyekaba <ekeneonyekaba@gmail.com>
2021-10-30 08:40:35 +0100

    Feature: update post route

--- model/post.go
+++ model/post.go
@@ -1,11 +1,45 @@
 package model
 
-import "github.com/Ekenzy-101/GCP-Serverless/types"
+import (
+	"time"
+
+	"cloud.google.com/go/firestore"
+	"github.com/Ekenzy-101/GCP-Serverless/types"
+)
 
 type Post struct {
-	ID      string  `json:"id,omitempty" firestore:"id,omitempty"`
-	Content string  `json:"content,omitempty" firestore:"content,omitempty" validate:"required"`
-	Image   string  `json:"image,omitempty" firestore:"image,omitempty"`
-	Title   string  `json:"title,omitempty" firestore:"title,omitempty" validate:"required"`
-	User    types.M `json:"user,omitempty" firestore:"user,omitempty"`
+	ID        string      `json:"id,omitempty" firestore:"id,omitempty"`
+	CreatedAt interface{} `json:"createdAt,omitempty" firestore:"createdAt,omitempty"`
+	Content   string      `json:"content,omitempty" firestore:"content,omitempty" validate:"required"`
+	Image     string      `json:"image,omitempty" firestore:"image,omitempty"`
+	Title     string      `json:"title,omitempty" firestore:"title,omitempty" validate:"required"`
+	User      types.M     `json:"user,omitempty" firestore:"user,omitempty"`
+	UpdatedAt interface{} `json:"updatedAt,omitempty" firestore:"updatedAt,omitempty"`
+}
+
+func NewPostFromDocument(document *firestore.DocumentSnapshot) (*Post, error) {
+	post := &Post{}
+	err := document.DataTo(post)
+	return post, err
+}
+
+func (p *Post) SetContent(value string) *Post {
+	p.Content = value
+	return p
+}
+
+func (p *Post) SetID(value string) *Post {
+	p.ID = value
+	return p
+}
+
+func (p *Post) SetTimestamps(createTime, updateTime time.Time) *Post {
+	p.CreatedAt = createTime
+	p.UpdatedAt = updateTime
+	return p
+}
+
+func (p *Post) SetTitle(value string) *Post {
+	p.Title = value
+	return p
 }
--- post.go
+++ post.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strings"
 
+	"cloud.google.com/go/firestore"
 	"github.com/Ekenzy-101/GCP-Serverless/config"
 	"github.com/Ekenzy-101/GCP-Serverless/helper"
 	"github.com/Ekenzy-101/GCP-Serverless/model"
@@ -36,7 +37,7 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	doc, err := client.Collection(config.UsersCollection).Doc(claims.Subject).Get(ctx)
+	userDocument, err := client.Collection(config.UsersCollection).Doc(claims.Subject).Get(ctx)
 	if status.Code(err) == codes.NotFound {
 		helper.SendJSONResponse(w, http.StatusNotFound, types.M{"message": "User not found"})
 		return
@@ -53,22 +54,22 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	docRef := client.Collection(config.PostsCollection).NewDoc()
-	url, err := service.GeneratePresignedURL(fmt.Sprintf("posts/%v", docRef.ID))
+	documentRef := client.Collection(config.PostsCollection).NewDoc()
+	url, err := service.GeneratePresignedURL(fmt.Sprintf("posts/%v", documentRef.ID))
 	if err != nil {
 		helper.SendJSONResponse(w, http.StatusInternalServerError, types.M{"message": err.Error()})
 		return
 	}
 
 	post.Image = strings.Split(url, "?")[0]
-	post.User = types.M{"id": claims.Subject, "name": doc.Data()["name"]}
-	_, err = docRef.Create(ctx, post)
+	post.User = types.M{"id": claims.Subject, "name": userDocument.Data()["name"]}
+	result, err := documentRef.Create(ctx, post)
 	if err != nil {
 		helper.SendJSONResponse(w, http.StatusInternalServerError, types.M{"message": err.Error()})
 		return
 	}
 
-	post.ID = docRef.ID
+	post = post.SetID(documentRef.ID).SetTimestamps(result.UpdateTime, result.UpdateTime)
 	helper.SendJSONResponse(w, http.StatusOK, types.M{"post": post, "url": url})
 }
 
@@ -85,7 +86,7 @@ func GetPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	doc, err := client.Collection(config.PostsCollection).Doc(postId).Get(ctx)
+	document, err := client.Collection(config.PostsCollection).Doc(postId).Get(ctx)
 	if status.Code(err) == codes.NotFound {
 		helper.SendJSONResponse(w, http.StatusNotFound, types.M{"message": "Post with the given id does not exist"})
 		return
@@ -96,8 +97,13 @@ func GetPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	post := doc.Data()
-	post["id"] = postId
+	post, err := model.NewPostFromDocument(document)
+	if err != nil {
+		helper.SendJSONResponse(w, http.StatusInternalServerError, types.M{"message": err.Error()})
+		return
+	}
+
+	post = post.SetID(postId).SetTimestamps(document.CreateTime, document.UpdateTime)
 	helper.SendJSONResponse(w, http.StatusOK, types.M{"post": post})
 }
 
@@ -106,5 +112,66 @@ func GetPosts(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdatePost(w http.ResponseWriter, r *http.Request) {
+	value, err := helper.AuthorizeRequest(r)
+	if err != nil {
+		helper.SendJSONResponse(w, http.StatusUnauthorized, types.M{"message": err.Error()})
+		return
+	}
 
+	claims, ok := value.(*jwt.RegisteredClaims)
+	if !ok {
+		helper.SendJSONResponse(w, http.StatusUnauthorized, types.M{"message": "Payload's format is invalid"})
+		return
+	}
+
+	requestBody := &model.Post{}
+	if messages := helper.ValidateRequestBody(r, requestBody); messages != nil {
+		helper.SendJSONResponse(w, http.StatusBadRequest, messages)
+		return
+	}
+
+	postId := r.URL.Query().Get("id")
+	ctx := context.Background()
+	client, err := service.CreateFirestoreClient(ctx)
+	if err != nil {
+		helper.SendJSONResponse(w, http.StatusInternalServerError, types.M{"message": err.Error()})
+		return
+	}
+
+	documentRef := client.Collection(config.PostsCollection).Doc(postId)
+	document, err := documentRef.Get(ctx)
+	if status.Code(err) == codes.NotFound {
+		helper.SendJSONResponse(w, http.StatusNotFound, types.M{"message": "Post with the given id does not exist"})
+		return
+	}
+
+	if err != nil {
+		helper.SendJSONResponse(w, http.StatusInternalServerError, types.M{"message": err.Error()})
+		return
+	}
+
+	post, err := model.NewPostFromDocument(document)
+	if err != nil {
+		helper.SendJSONResponse(w, http.StatusInternalServerError, types.M{"message": err.Error()})
+		return
+	}
+
+	if post.User["id"] != claims.Subject {
+		helper.SendJSONResponse(w, http.StatusForbidden, types.M{"message": "You are not allowed to update this post"})
+		return
+	}
+
+	updates := []firestore.Update{
+		{Path: "title", Value: requestBody.Title},
+		{Path: "content", Value: requestBody.Content},
+	}
+	result, err := documentRef.Update(ctx, updates)
+	if err != nil {
+		helper.SendJSONResponse(w, http.StatusInternalServerError, types.M{"message": err.Error()})
+		return
+	}
+
+	post = post.SetContent(requestBody.Content).SetID(postId).
+		SetTimestamps(document.CreateTime, result.UpdateTime).SetTitle(requestBody.Title)
+	helper.SendJSONResponse(w, http.StatusOK, types.M{"post": post})
 }
